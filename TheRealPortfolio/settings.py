632e0a5483d34654d40e307cdbf465c4ceeddc48
Ubuntu <ubuntu@ip-172-31-23-209.ec2.internal>
2017-05-10 19:43:40 +0000

    edited about.html

--- TheRealPortfolio/settings.py
+++ TheRealPortfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_ou!77*8)2@t&-q*cxio2g$dyvg(5y$!!9314r@^ah(1ygfgmk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.202.186.89']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
